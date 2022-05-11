770414cb01781dbcded64765e5a15aba5989bf5e
Shen Li <shenlirobot@gmail.com>
2022-05-11 17:55:57 -0400

    add dressing init state controller

---
+++ src/libfranka_joint_goal_motion_generator_dressing.cpp
@@ -0,0 +1,88 @@
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+
+// Easy non-franka_ros executable to send robot to desired joint configurations. 
+// Caution: It won't work when franka_ros/franka_control is running!
+
+#include <array>
+#include <atomic>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <mutex>
+#include <thread>
+
+#include <franka/duration.h>
+#include <franka/exception.h>
+#include <franka/model.h>
+#include <franka/rate_limiting.h>
+#include <franka/robot.h>
+
+// #include <franka_motion_generators/libfranka_joint_motion_generator.h>
+#include <libfranka_joint_motion_generator.h>
+
+namespace {
+template <class T, size_t N>
+std::ostream& operator<<(std::ostream& ostream, const std::array<T, N>& array) {
+  ostream << "[";
+  std::copy(array.cbegin(), array.cend() - 1, std::ostream_iterator<T>(ostream, ","));
+  std::copy(array.cend() - 1, array.cend(), std::ostream_iterator<T>(ostream));
+  ostream << "]";
+  return ostream;
+}
+}  // anonymous namespace
+
+int main(int argc, char** argv) {
+
+  std::string franka_ip = "172.16.0.2";
+
+  // Check whether the required arguments were passed replace this with rosparam!
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <goal_id>" << std::endl;
+    return -1;
+  }
+
+  try {
+    
+    // Connect to robot.
+    franka::Robot robot(franka_ip);
+
+    // Set additional parameters always before the control loop, NEVER in the control loop!
+    // Set collision behavior.
+    robot.setCollisionBehavior(
+        {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}}, {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+        {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}}, {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
+        {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}}, {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+        {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}}, {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}});
+
+
+    // First move the robot to a suitable joint configuration
+    int goal_id = std::stod(argv[1]);
+
+    std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
+    switch(goal_id) {
+       case 0  :
+          std::cout << "Selected q_home as goal" << std::endl;
+          q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
+          break;
+       case 1  :
+        std::cout << "1" << std::endl;
+          q_goal = {{-0.5960621641630317, 0.2800048621664451, 0.09245445224590469, -1.8320575581768104, -0.04451331242377911, 2.1618705587217018, -2.337254942422769}};
+          break;
+    }
+
+    MotionGenerator motion_generator(0.6, q_goal);
+    std::cout << "WARNING: This example will move the robot! "
+              << "Please make sure to have the user stop button at hand!" << std::endl
+              << "Press Enter to continue..." << std::endl;
+    std::cin.ignore();
+    robot.control(motion_generator);
+    std::cout << "Finished moving to initial joint configuration." << std::endl;
+
+  } catch (const franka::Exception& ex) {
+    std::cerr << ex.what() << std::endl;
+  }
+
+  return 0;
+}
